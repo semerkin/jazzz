42e89a61e95ccdebb0ba24172066c693e7c76345
semerkin <semerkin771@gmail.com>
2020-06-29 16:17:23 +0500

    Update timerange.java

--- timerange.java
+++ timerange.java
@@ -1,5 +1,31 @@
-// open your connection
-long start = System.currentTimeMillis();
-// send request, wait for response (the simple socket calls are all blocking)
-long end = System.currentTimeMillis();
-System.out.println("Round trip response time = " + (end - start) + " millis");
+int max = 1000000;
+List<String> values = new ArrayList<>(max);
+for (int i = 0; i < max; i++) {
+    UUID uuid = UUID.randomUUID();
+    values.add(uuid.toString());
+}
+
+long t0 = System.nanoTime();
+
+long count = values.stream().sorted().count();
+System.out.println(count);
+
+long t1 = System.nanoTime();
+
+long millis = TimeUnit.NANOSECONDS.toMillis(t1 - t0);
+System.out.println(String.format("sequential sort took: %d ms", millis));
+
+// sequential sort took: 899 ms
+
+long t0 = System.nanoTime();
+
+long count = values.parallelStream().sorted().count();
+System.out.println(count);
+
+long t1 = System.nanoTime();
+
+long millis = TimeUnit.NANOSECONDS.toMillis(t1 - t0);
+System.out.println(String.format("parallel sort took: %d ms", millis));
+
+// parallel sort took: 472 ms
+
